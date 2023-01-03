99ad44a220b648035360ef515a11df9d3ed9d35f
Davis Kauffmann <daviskauffmann@gmail.com>
2023-01-03 16:33:36 -0500

    Audio defaults

--- chmain/src/main.cpp
+++ chmain/src/main.cpp
@@ -14,7 +14,7 @@ int main(int, char *[])
     const ch::sdl sdl(SDL_INIT_AUDIO | SDL_INIT_VIDEO);
     const ch::sdl_image sdl_image;
     const ch::sdl_mixer sdl_mixer;
-    const ch::audio audio(MIX_DEFAULT_FREQUENCY, MIX_DEFAULT_FORMAT, MIX_DEFAULT_CHANNELS, 4096);
+    const ch::audio audio;
     const ch::sdl_ttf sdl_ttf;
     const ch::enet enet;
 
